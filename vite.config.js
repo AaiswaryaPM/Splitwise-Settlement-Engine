f43a6f14663715e5e53a85bde8bf387aceb3d9cd
AaiswaryaPM <aaiswaribm.learning@gmail.com>
2026-04-23 21:16:26 +0530

    Added new changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Splitwise-Settlement-Engine/", // ADD THIS LINE (Match your repo name)
+  base: "/Splitwise-Settlement-Engine/",
 })
